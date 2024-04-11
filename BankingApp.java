d8fa3c63fa2f630411721b88b7c158371553e3e2
HarshSharma1246 <142594711+HarshSharma1246@users.noreply.github.com>
2024-04-11 15:25:38 +0530

    Update BankingApp.java

--- BankingApp.java
+++ BankingApp.java
@@ -8,6 +8,5 @@ public class BankingApp {
                 "\AccountData.txt");
         BankDetails.addFile(myFile); // Adding the Saved Account data file
         new HomePage(); // Opening the interface
-        BankDetails.displayFileContents(myFile);
     }
 }
